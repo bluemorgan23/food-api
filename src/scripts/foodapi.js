3633223766964d18ed19d33308540b9da269bd5b
Chris Morgan <bluemorgan2323@gmail.com>
2019-03-06 09:11:23 -0600

    Created normal project setup. Fetch the food api and create two functions which ultimately render the objects from this api to the DOM

---
+++ src/scripts/foodapi.js
@@ -0,0 +1,39 @@
+let element = document.createElement("div");
+element.classList.add("foodList");
+document.body.appendChild(element);
+let foodLog = document.querySelector(".foodList")
+
+const builder = (insertMe) => {
+    return `
+        <h1>${insertMe.name}</h1>
+        <p>${insertMe.category}</p>
+        <p>${insertMe.ethnicity}</p>
+    `;
+} 
+
+const render = (item) => {
+    foodLog.innerHTML += builder(item);
+    console.log(item);
+}
+
+fetch("http://localhost:8088/food")
+    .then(food => food.json())
+    .then(parsedFood => {
+        parsedFood.forEach(food => {
+            render(food)
+        })
+    });
+
+// Alternative way to write this code, function expressions  
+// instead of arrow 
+
+// fetch("http://localhost:8088/food")
+//     .then(function(foods) {
+//         return foods.json();
+//     })
+//     .then(function (parsedFoods) {
+//         console.table(parsedFoods);
+//     });
+
+
+
